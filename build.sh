27266caf70026ce1d02c262f00b454f3eefe7289
Humza Chouhdry <hc345@duke.edu>
2024-09-08 04:59:46 -0400

    added graphics for aliens

--- build.sh
+++
@@ -1,2 +0,0 @@
-rm *.o
-g++ -o spacewars src/main.cpp src/spaceship.cpp -I/opt/homebrew/Cellar/raylib/5.0/include -L/opt/homebrew/Cellar/raylib/5.0/lib -lraylib -framework OpenGL -framework Cocoa -framework IOKit -framework CoreVideo -std=c++11 -arch arm64
